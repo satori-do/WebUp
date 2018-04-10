1800e60b094fc58537fac2a66a06e2929652d123
satori-do <volodymyrreznich@gmail.com>
2018-04-10 17:04:33 +0300

    Abbreviated script without group docker

---
+++ provision-script.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+apt-get  update -y
+apt-get -y upgrade
+apt-get install git -y
+apt-get install htop -y
+apt-get install mc -y
+apt-get install -y apt-transport-https ca-certificates curl software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
+add-apt-repository -y "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+apt-get update
+apt-get install -y docker-ce
+docker run hello-world
+apt-get update
+cd /var/www/html
+
+#if [ ! -e rename.kr.ua ]; then
+#git clone https://github.com/Onix-Systems/rename.kr.ua.git
+#else
+#cd rename.kr.ua
+#git pull
+#fi
+
+# sudo docker pull nixes/ubuntu-lamp
+# docker build -t="leafney/ubuntu-mysql" .
+# docker run --name mysql -v /var/docker_data/mysql/data/:/var/lib/mysql -d -p 3306:3306 leafney/ubuntu-mysql
+
+# apt-get purge docker-ce
+# rm -rf /var/lib/docker
+
+
+
