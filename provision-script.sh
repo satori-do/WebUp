8840f240f0afac145f05299b847590548a084ea7
satori-do <volodymyrreznich@gmail.com>
2018-04-12 16:30:53 +0300

    Script command modification

--- provision-script.sh
+++ provision-script.sh
@@ -2,17 +2,24 @@
 
 apt-get update -y
 apt-get upgrade -y
-apt-get install git -y
-apt-get install htop -y
-apt-get install mc -y
+apt-get install -y \
+       git \
+       htop \
+       mc
+#apt-get install git -y
+#apt-get install htop -y
+#apt-get install mc -y
 apt-get install -y apt-transport-https ca-certificates curl software-properties-common
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
 add-apt-repository -y "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
 apt-get update
-apt-get install -y docker-ce -y
+apt-get install -y docker-ce
 
 #sudo groupadd docker
-sudo gpasswd -a vagrant docker
+
+sudo usermod -aG docker your-user
+#sudo gpasswd -a vagrant docker
+
 #echo $USER
 #sudo newgrp docker
 
