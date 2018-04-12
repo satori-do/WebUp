df61b28340d8339e1bd93704a353a69075d713fd
satori-do <volodymyrreznich@gmail.com>
2018-04-12 09:36:08 +0300

    Some adjustmens...

--- provision-script.sh
+++ provision-script.sh
@@ -9,14 +9,16 @@ apt-get install -y apt-transport-https ca-certificates curl software-properties-
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
 add-apt-repository -y "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
 apt-get update
-apt-get install -y docker-ce
+apt-get install -y docker-ce -y
 
 #sudo groupadd docker
 sudo gpasswd -a vagrant docker
-echo $USER
+#echo $USER
 #sudo newgrp docker
 
-docker run hello-world
+#docker run hello-world
+
+docker pull mysql
 #apt-get update
 #cd /var/www/html
 
