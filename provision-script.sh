9c965a01a49aec61852684bba95510ec33a6cd4f
satori-do <volodymyrreznich@gmail.com>
2018-04-13 14:08:03 +0300

    Add PHPAch..

--- provision-script.sh
+++ provision-script.sh
@@ -23,7 +23,7 @@ git pull
 fi
 
 docker run --name rename-db -v /var/lib/mysql:/var/lib/mysql -v /vagrant/rmkr.sql:/docker-entrypoint-initdb.d/rmkr.sql:ro -eMYSQL_DATABASE=rmkr -e MYSQL_ROOT_PASSWORD=123456 -d mysql:5.7 --character-set-server=utf8 --collation-server=utf8_general_ci
-
+docker run -d -p 80:80 --name rename.kr.ua -v "$PWD":/var/www/html php:7.0-apache
 
 # apt-get purge docker-ce
 # rm -rf /var/lib/docker
