6ef0791e669031c0ca0283f4212ebae82196a804
satori-do <volodymyrreznich@gmail.com>
2018-04-13 15:18:47 +0300

    Optimisations code & path

--- provision-script.sh
+++ provision-script.sh
@@ -1,17 +1,15 @@
 #!/bin/bash
 
+apt-get install -y apt-transport-https ca-certificates curl software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
+add-apt-repository -y "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
 apt-get update -y
 apt-get upgrade -y
 apt-get install -y \
        git \
        htop \
-       mc
-
-apt-get install -y apt-transport-https ca-certificates curl software-properties-common
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
-add-apt-repository -y "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
-apt-get update
-apt-get install -y docker-ce
+       mc \
+       docker-ce
 
 sudo gpasswd -a vagrant docker
 
@@ -23,7 +21,7 @@ git pull
 fi
 
 docker run --name rename-db -v /var/lib/mysql:/var/lib/mysql -v /vagrant/rmkr.sql:/docker-entrypoint-initdb.d/rmkr.sql:ro -eMYSQL_DATABASE=rmkr -e MYSQL_ROOT_PASSWORD=123456 -d mysql:5.7 --character-set-server=utf8 --collation-server=utf8_general_ci
-docker run -d -p 80:80 --name rename.kr.ua -v "$PWD":/var/www/html php:7.0-apache
+docker run -d -p 80:80 --name rename.kr.ua -v /home/vagrant/rename.kr.ua:/var/www/html php:7.0-apache
 
 # apt-get purge docker-ce
 # rm -rf /var/lib/docker
