f49befd4d960de3c51da8c39ecd9b518637e4d4e
satori-do <volodymyrreznich@gmail.com>
2018-04-17 18:15:30 +0300

    Debug

--- provision-script.sh
+++ provision-script.sh
@@ -17,16 +17,19 @@ sudo usermod -aG docker vagrant
 if [ ! -e rename.kr.ua ]; then
 git clone https://github.com/Onix-Systems/rename.kr.ua.git
 else
-cd rename.kr.ua
+cd /home/vagrant/rename.kr.ua/
 git pull
 fi
 
 cp /vagrant/Dockerfile  /home/vagrant/rename.kr.ua/
 cp /vagrant/config.php  /home/vagrant/rename.kr.ua/config/
-cd /rename.kr.ua/
 
 docker pull mysql:5.7
 docker pull php:7.0-apache
+
+cd /home/vagrant/rename.kr.ua/
+
 docker build -t phpach .
+
 docker run --name rename-db -v /var/lib/mysql:/var/lib/mysql -v /vagrant/rmkr.sql:/docker-entrypoint-initdb.d/rmkr.sql:ro -e MYSQL_ROOT_PASSWORD_FILE=/vagrant/mysql-root -d mysql:5.7
 docker run -d -p 80:80 --name rename.kr.ua --link rename-db:db phpach:latest
