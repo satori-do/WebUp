e48b39f7737c101d53876b6bf6710c5d4799efe5
satori-do <volodymyrreznich@gmail.com>
2018-04-17 11:16:48 +0300

    Add --link

--- provision-script.sh
+++ provision-script.sh
@@ -22,10 +22,10 @@ git pull
 fi
 
 cp -a /vagrant/*  /home/vagrant/rename.kr.ua/
-cd rename.kr.ua/
+cd /rename.kr.ua/
 
 docker pull mysql:5.7
 docker pull php:7.0-apache
 docker build -t phpach .
 #docker run --name rename-db -v /var/lib/mysql:/var/lib/mysql -v /vagrant/rmkr.sql:/docker-entrypoint-initdb.d/rmkr.sql:ro -eMYSQL_DATABASE=rmkr -e MYSQL_ROOT_PASSWORD=123456 -d mysql:5.7 --character-set-server=utf8 --collation-server=utf8_general_ci
-#docker run -d -p 80:80 --name rename.kr.ua phpach:latest
+#docker run -d -p 80:80 --name rename.kr.ua --link rename-db:db phpach:latest
