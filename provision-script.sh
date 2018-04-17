188fa55a5d1c486194d4c5472b1c5588a4710033
satori-do <volodymyrreznich@gmail.com>
2018-04-17 16:43:34 +0300

    Working script

--- provision-script.sh
+++ provision-script.sh
@@ -21,11 +21,12 @@ cd rename.kr.ua
 git pull
 fi
 
-cp -a /vagrant/*  /home/vagrant/rename.kr.ua/
+cp /vagrant/Dockerfile  /home/vagrant/rename.kr.ua/
+cp /vagrant/config.php  /home/vagrant/rename.kr.ua/config/
 cd /rename.kr.ua/
 
 docker pull mysql:5.7
 docker pull php:7.0-apache
 docker build -t phpach .
-#docker run --name rename-db -v /var/lib/mysql:/var/lib/mysql -v /vagrant/rmkr.sql:/docker-entrypoint-initdb.d/rmkr.sql:ro -eMYSQL_DATABASE=rmkr -e MYSQL_ROOT_PASSWORD=123456 -d mysql:5.7 --character-set-server=utf8 --collation-server=utf8_general_ci
-#docker run -d -p 80:80 --name rename.kr.ua --link rename-db:db phpach:latest
+docker run --name rename-db -v /var/lib/mysql:/var/lib/mysql -v /vagrant/rmkr.sql:/docker-entrypoint-initdb.d/rmkr.sql:ro -e MYSQL_ROOT_PASSWORD_FILE=/vagrant/mysql-root -d mysql:5.7
+docker run -d -p 80:80 --name rename.kr.ua --link rename-db:db phpach:latest
