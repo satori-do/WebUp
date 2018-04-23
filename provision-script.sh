549f2b7470e99a8650409e73aef3f61efa3893e1
satori-do <volodymyrreznich@gmail.com>
2018-04-23 14:48:49 +0300

    Add copy inside

--- provision-script.sh
+++ provision-script.sh
@@ -17,33 +17,31 @@ apt-get install -y \
 sudo usermod -aG docker vagrant
 
 if [ ! -e rename.kr.ua ]; then
-    git clone https://github.com/Onix-Systems/rename.kr.ua.git
+    git clone https://github.com/Onix-Systems/rename.kr.ua.git \
+    cp /vagrant/Dockerfile  /home/vagrant/rename.kr.ua/ \
+    cp /vagrant/rmkr.sql  /home/vagrant/rename.kr.ua/ \
+    cd /home/vagrant/rename.kr.ua/
 else
     cd /home/vagrant/rename.kr.ua/
     git pull
 fi
 
-cp /vagrant/Dockerfile  /home/vagrant/rename.kr.ua/
-
 docker pull mysql:5.7
 docker pull php:7.0-apache
-
-cd /home/vagrant/rename.kr.ua/
 
 CMYSQL=rename-db
 CWEB=rename.kr.ua
 
 docker rm -f ${CMYSQL} ${CWEB}
 
-
 docker build -t phpach .
 
 docker run --name ${CMYSQL} -v /var/lib/mysql:/var/lib/mysql \
-           -v /vagrant/rmkr.sql:/docker-entrypoint-initdb.d/rmkr.sql:ro \
-           --env-file /vagrant/credential -e MYSQL_RANDOM_ROOT_PASSWORD=yes \
+           -v $PWD/rmkr.sql:/docker-entrypoint-initdb.d/rmkr.sql:ro \
+           --env-file $PWD/credential -e MYSQL_RANDOM_ROOT_PASSWORD=yes \
            --restart always \
            -d mysql:5.7
 
 docker run -d --restart always --name ${CWEB} -p 80:80 \
-           --link ${CMYSQL}:db --env-file /vagrant/credential \
+           --link ${CMYSQL}:db --env-file $PWD/credential \
            phpach:latest
