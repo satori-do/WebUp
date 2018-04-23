f4cbf04e2a3b07533f3492dbcb3f320bac552e62
satori-do <volodymyrreznich@gmail.com>
2018-04-23 15:13:47 +0300

    Add &&...

--- provision-script.sh
+++ provision-script.sh
@@ -17,10 +17,10 @@ apt-get install -y \
 sudo usermod -aG docker vagrant
 
 if [ ! -e rename.kr.ua ]; then
-    git clone https://github.com/Onix-Systems/rename.kr.ua.git \
+    git clone https://github.com/Onix-Systems/rename.kr.ua.git && \
     cp /vagrant/Dockerfile  /home/vagrant/rename.kr.ua/ \
     cp /vagrant/rmkr.sql  /home/vagrant/rename.kr.ua/ \
-    cp /vagrant/credential  /home/vagrant/rename.kr.ua/ \
+    cp /vagrant/crdential  /home/vagrant/rename.kr.ua/ && \
     cd /home/vagrant/rename.kr.ua/
 else
     cd /home/vagrant/rename.kr.ua/
